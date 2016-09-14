e2b38dd986a608ebf12a30e56830ecc9f891614b
kwanlekha <kwanlekha@gmail.com>
2016-09-14 16:56:35 +0700

    start project

---
+++ SimplePaint/SimplePaint/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace SimplePaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.panelToDraw = new System.Windows.Forms.Panel();
+            this.groupBoxToDraw = new System.Windows.Forms.GroupBox();
+            this.labelNumberToDraw = new System.Windows.Forms.Label();
+            this.labelShowMessage = new System.Windows.Forms.Label();
+            this.labelDraw = new System.Windows.Forms.Label();
+            this.buttonGetData = new System.Windows.Forms.Button();
+            this.groupBoxShowData = new System.Windows.Forms.GroupBox();
+            this.richTextBoxShowData = new System.Windows.Forms.RichTextBox();
+            this.groupBoxToDraw.SuspendLayout();
+            this.groupBoxShowData.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(56, 438);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 39);
+            this.buttonSave.TabIndex = 0;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(14, 396);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 29);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // panelToDraw
+            // 
+            this.panelToDraw.BackColor = System.Drawing.Color.White;
+            this.panelToDraw.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panelToDraw.Location = new System.Drawing.Point(14, 55);
+            this.panelToDraw.Name = "panelToDraw";
+            this.panelToDraw.Size = new System.Drawing.Size(160, 320);
+            this.panelToDraw.TabIndex = 2;
+            this.panelToDraw.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelToDraw_MouseDown);
+            this.panelToDraw.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelToDraw_MouseMove);
+            this.panelToDraw.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelToDraw_MouseUp);
+            // 
+            // groupBoxToDraw
+            // 
+            this.groupBoxToDraw.Controls.Add(this.labelNumberToDraw);
+            this.groupBoxToDraw.Controls.Add(this.labelShowMessage);
+            this.groupBoxToDraw.Controls.Add(this.labelDraw);
+            this.groupBoxToDraw.Controls.Add(this.panelToDraw);
+            this.groupBoxToDraw.Controls.Add(this.buttonSave);
+            this.groupBoxToDraw.Controls.Add(this.buttonGetData);
+            this.groupBoxToDraw.Controls.Add(this.buttonClear);
+            this.groupBoxToDraw.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxToDraw.Name = "groupBoxToDraw";
+            this.groupBoxToDraw.Size = new System.Drawing.Size(190, 526);
+            this.groupBoxToDraw.TabIndex = 3;
+            this.groupBoxToDraw.TabStop = false;
+            this.groupBoxToDraw.Text = "Draw Character";
+            // 
+            // labelNumberToDraw
+            // 
+            this.labelNumberToDraw.AutoSize = true;
+            this.labelNumberToDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.labelNumberToDraw.Location = new System.Drawing.Point(106, 25);
+            this.labelNumberToDraw.Name = "labelNumberToDraw";
+            this.labelNumberToDraw.Size = new System.Drawing.Size(18, 20);
+            this.labelNumberToDraw.TabIndex = 8;
+            this.labelNumberToDraw.Text = "0";
+            // 
+            // labelShowMessage
+            // 
+            this.labelShowMessage.AutoSize = true;
+            this.labelShowMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.labelShowMessage.Location = new System.Drawing.Point(6, 492);
+            this.labelShowMessage.Name = "labelShowMessage";
+            this.labelShowMessage.Size = new System.Drawing.Size(45, 16);
+            this.labelShowMessage.TabIndex = 7;
+            this.labelShowMessage.Text = "label2";
+            this.labelShowMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelDraw
+            // 
+            this.labelDraw.AutoSize = true;
+            this.labelDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.labelDraw.Location = new System.Drawing.Point(63, 27);
+            this.labelDraw.Name = "labelDraw";
+            this.labelDraw.Size = new System.Drawing.Size(45, 15);
+            this.labelDraw.TabIndex = 6;
+            this.labelDraw.Text = "Draw : ";
+            // 
+            // buttonGetData
+            // 
+            this.buttonGetData.Location = new System.Drawing.Point(99, 396);
+            this.buttonGetData.Name = "buttonGetData";
+            this.buttonGetData.Size = new System.Drawing.Size(75, 29);
+            this.buttonGetData.TabIndex = 4;
+            this.buttonGetData.Text = "GetData";
+            this.buttonGetData.UseVisualStyleBackColor = true;
+            this.buttonGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // groupBoxShowData
+            // 
+            this.groupBoxShowData.Controls.Add(this.richTextBoxShowData);
+            this.groupBoxShowData.Location = new System.Drawing.Point(208, 12);
+            this.groupBoxShowData.Name = "groupBoxShowData";
+            this.groupBoxShowData.Size = new System.Drawing.Size(801, 526);
+            this.groupBoxShowData.TabIndex = 5;
+            this.groupBoxShowData.TabStop = false;
+            this.groupBoxShowData.Text = "Show Data";
+            // 
+            // richTextBoxShowData
+            // 
+            this.richTextBoxShowData.Font = new System.Drawing.Font("Courier New", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxShowData.Location = new System.Drawing.Point(16, 22);
+            this.richTextBoxShowData.Name = "richTextBoxShowData";
+            this.richTextBoxShowData.ReadOnly = true;
+            this.richTextBoxShowData.Size = new System.Drawing.Size(769, 486);
+            this.richTextBoxShowData.TabIndex = 5;
+            this.richTextBoxShowData.Text = "";
+            this.richTextBoxShowData.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 550);
+            this.Controls.Add(this.groupBoxShowData);
+            this.Controls.Add(this.groupBoxToDraw);
+            this.Name = "Form1";
+            this.Text = "Handwriting Recognition";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.groupBoxToDraw.ResumeLayout(false);
+            this.groupBoxToDraw.PerformLayout();
+            this.groupBoxShowData.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Panel panelToDraw;
+        private System.Windows.Forms.GroupBox groupBoxToDraw;
+        private System.Windows.Forms.Button buttonGetData;
+        private System.Windows.Forms.GroupBox groupBoxShowData;
+        private System.Windows.Forms.RichTextBox richTextBoxShowData;
+        private System.Windows.Forms.Label labelDraw;
+        private System.Windows.Forms.Label labelShowMessage;
+        private System.Windows.Forms.Label labelNumberToDraw;
+    }
+}
+
---
+++ SimplePaint/SimplePaint/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+//-----command that use in sql just like select, count
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimplePaint
+{
+    public partial class Form1 : Form
+    {
+        bool paint = false;
+        SolidBrush color;
+        int numberToDraw = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            labelShowMessage.Text = "--- Drawing and input name ---";
+            labelShowMessage.ForeColor = Color.Blue;
+        }
+
+        private void panelToDraw_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+        }
+
+        private void panelToDraw_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+        }
+
+        private void panelToDraw_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (paint)
+            {
+                color = new SolidBrush(Color.Black);
+                //-----drawing graphics
+                using(Graphics g = panelToDraw.CreateGraphics())
+                {
+                    //-----fill ellipse with color which e is mouse, e.X is mouse click at the current X position
+                    g.FillEllipse(color, e.X, e.Y, 10, 10);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clearScreen();
+        }
+
+        private void clearScreen()
+        {
+            clearPanel();
+            richTextBoxShowData.Clear();
+        }
+
+        private void clearPanel()
+        {
+            Graphics g1 = panelToDraw.CreateGraphics();
+            g1.Clear(panelToDraw.BackColor);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            save();
+        }
+
+        private void save()
+        {
+            //-----create bitmap with width and height are equal to panel1
+            Bitmap bmp = new Bitmap(panelToDraw.Width, panelToDraw.Height);
+            //-----use using like dispose function to clean up all resources
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                //-----fix sixe and location of panel1 in screen
+                Rectangle rect = panelToDraw.RectangleToScreen(panelToDraw.ClientRectangle);
+                //-----copy panel1 from screen
+                g.CopyFromScreen(rect.Location, Point.Empty, panelToDraw.Size);
+            }
+
+            //-----save image into bmp
+            try
+            {
+                string directory_name = labelNumberToDraw.Text;
+                if (!Directory.Exists(directory_name))
+                {
+                    Directory.CreateDirectory(directory_name);
+                }
+                //labelNumberToDraw.text is a name of directory
+                string[] filenames = Directory.GetFiles(directory_name);
+                string filename = "0.bmp";
+                if (filenames.Length != 0)
+                {
+                    int maxNum = filenames.Where(a => 
+                                        {
+                                            try
+                                            {
+                                                Convert.ToInt32(Path.GetFileNameWithoutExtension(a));
+                                                return true;
+                                            }
+                                            catch (Exception)
+                                            {
+                                                return false;
+                                            }
+                                        })
+                                        .Max(a => {
+                                            return Convert.ToInt32(Path.GetFileNameWithoutExtension(a));
+                                        });
+                    filename = (maxNum + 1) + ".bmp";
+                }
+                bmp.Save(Path.GetFullPath(directory_name) + "/" + filename, ImageFormat.Bmp);
+                labelShowMessage.Text = "--- Image "+ filename +" saved ---";
+                labelShowMessage.ForeColor = Color.Blue;
+                //MessageBox.Show("Image saved successfully.");
+                clearPanel();
+                richTextBoxShowData.Clear();
+                numberToDraw ++;
+                if (numberToDraw > 9)
+                {
+                    numberToDraw = 0;
+                }
+                labelNumberToDraw.Text = numberToDraw.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Image not save: " + ex.Message);
+            }
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            //-----create bitmap with width and height are equal to panel1
+            Bitmap bmp = new Bitmap(panelToDraw.Width, panelToDraw.Height);
+            //-----use using like dispose function to clean up all resources
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                //-----fix sixe and location of panel1 in screen
+                Rectangle rect = panelToDraw.RectangleToScreen(panelToDraw.ClientRectangle);
+                //-----copy panel1 from screen
+                g.CopyFromScreen(rect.Location, Point.Empty, panelToDraw.Size);
+            }
+
+            //-----get value of each pixel
+            int[][] pixels = new int[bmp.Height][];
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                pixels[y] = new int[bmp.Width];
+            }
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    if (bmp.GetPixel(x, y).GetBrightness() < 0.5)
+                    {
+                        //-----one refers to black
+                        pixels[y][x] = 1;
+                    }
+                    else
+                    {
+                        //-----zero refers to white
+                        pixels[y][x] = 0;
+                    }
+                }
+            }
+            //put value in each pixel into array and not show value of pixels == 0, show value of pixels == 1 
+            string[] ids = pixels.Select(a => String.Join("", a.Select(b => b == 0 ? " " : "1"))).ToArray();
+            //string[] ids = pixels.Select(a => String.Join("", a).ToArray();
+            richTextBoxShowData.Lines = ids;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                save();
+            }
+            else if (e.KeyCode == Keys.Back)
+            {
+                clearScreen();
+            }
+        }
+        
+    }
+}
